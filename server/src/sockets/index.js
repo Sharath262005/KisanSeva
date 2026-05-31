c440ed9176ac78067a21c18652e8b0c2d0a81697
Sharath <sharathakki4@gmail.com>
2026-05-31 17:35:35 +0530

    Add GPS tracking, location input mode, invoice fixes, and UI improvements

--- server/src/sockets/index.js
+++ server/src/sockets/index.js
@@ -8,6 +8,16 @@ module.exports = {
     io.on('connection', (socket) => {
       console.log('Client connected:', socket.id);
       socket.on('join', (userId) => socket.join(`user_${userId}`));
+      // Driver joins booking room and sends location
+      socket.on('join_booking_room', (bookingId) => {
+        socket.join(`booking_${bookingId}`);
+      });
+
+      socket.on('driver_location_update', ({ bookingId, latitude, longitude }) => {
+        // Broadcast to all clients in that booking room (including the farmer)
+        io.to(`booking_${bookingId}`).emit('driver_location', { latitude, longitude });
+        // Optionally save to database GPS logs (we can add later)
+      });
     });
     return io;
   },
